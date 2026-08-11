6ad1ef52a44dc4803777b2591fe4e0409459c9ae
Martinica Andrei-Marian <ni3r4ut0m4t4@gmail.com>
2026-08-11 20:00:11 +0300

    implement modal functionality

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 let $ = document.querySelector.bind(document);
+const header = $("#header");
 const menu = $("#menu");
 
 const toggleMenu = (e) =>{
+    const headerClass = "header-full";
+    const overflowHiddenClass = "overflow-hidden";
     if (menu.open){
         menu.close();
+        header.classList.remove(headerClass);
+        document.documentElement.classList.remove(overflowHiddenClass);
     }
     else{
         menu.show();
+        header.classList.add(headerClass);
+        document.documentElement.classList.add(overflowHiddenClass);
     }
 }
